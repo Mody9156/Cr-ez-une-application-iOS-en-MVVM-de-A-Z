688d01357f0c2422563ed4011285624e1059a37e
Modibo KEITA <modykeita2@gmail.com>
2024-05-14 15:39:46 +0200

    update background and button

--- Vitesse/Vitesse/View/Login.swift
+++ Vitesse/Vitesse/View/Login.swift
@@ -13,42 +13,45 @@ struct Login: View {
     
     
     var body: some View {
-        VStack {
-            
-                Text("Login")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.blue).padding()
-            
-            VStack{
-                    Text("Email/Username")
-                    TextField("Entrez un Email ou Username valide", text: $username).disableAutocorrection(true)
+        ZStack {
+            Color.blue.opacity(0.5).ignoresSafeArea()
+            VStack {
+                
+                    Text("Login")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.blue).padding()
+                
+                VStack{
+                    Text("Email/Username").foregroundColor(.white)
+                        TextField("Entrez un Email ou Username valide", text: $username).disableAutocorrection(true)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                VStack{
+                        Text("Password").foregroundColor(.white)
+                        .font(.title3)
+                        SecureField("Veuillez entrez un mot de passe vaide", text: $password)
                         .textFieldStyle(.roundedBorder)
-                }
-            VStack{
-                    Text("Password")
-                    .font(.title3)
-                    SecureField("Veuillez entrez un mot de passe vaide", text: $password)
-                    .textFieldStyle(.roundedBorder)
+                        
+                }.padding()
+                
+                Button("Sign in") {
                     
-            }.padding()
-            
-            Button("Sign in") {
+                }.frame(width: 100,height: 50).foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(10)
                 
-            }.frame(width: 100,height: 50).foregroundColor(.white)
-                .background(.blue)
-                .cornerRadius(15)
-            
-            Button("Registre") {
-                
-            }.frame(width: 100,height: 50)
-                .background(.blue)
-                .foregroundColor(.white)
-                .cornerRadius(15)
+                Button("Registrer") {
+                    
+                }.frame(width: 100,height: 50)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
 
-            
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
