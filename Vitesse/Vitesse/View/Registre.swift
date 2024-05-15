8243fcb83e6f64c36779a9b1e1f384a3acff202b
Modibo KEITA <modykeita2@gmail.com>
2024-05-15 17:18:51 +0200

    add new view Registre

---
+++ Vitesse/Vitesse/View/Registre.swift
@@ -0,0 +1,18 @@
+//
+//  Registre.swift
+//  Vitesse
+//
+//  Created by KEITA on 15/05/2024.
+//
+
+import SwiftUI
+
+struct Registre: View {
+    var body: some View {
+        Text("Registre")
+        .font(.largeTitle)
+        .fontWeight(.bold)
+        .foregroundColor(.blue).padding()
+    }
+}
+
