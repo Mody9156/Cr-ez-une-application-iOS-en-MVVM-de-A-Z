ef737dcec6182e8ead133b9a36862ff7d0363220
Modibo KEITA <modykeita2@gmail.com>
2024-05-15 17:29:08 +0200

    update view Registre.swift

--- Vitesse/Vitesse/View/Registre.swift
+++ Vitesse/Vitesse/View/Registre.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct Registre: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+   
     var body: some View {
-        Text("Registre")
-        .font(.largeTitle)
-        .fontWeight(.bold)
+        VStack {
+            
+            Text("Registre")
+            .font(.largeTitle)
+            .fontWeight(.bold)
         .foregroundColor(.blue).padding()
+            
+            VStack{
+                
+                Text("First Name").foregroundColor(.white)
+                TextField("Use First Name valid", text: $firstName)
+                    .disableAutocorrection(true)
+                    .textFieldStyle(.roundedBorder).padding()
+             
+                Text("Last Name").foregroundColor(.white)
+                TextField("Use Last Name valid", text: $lastName)
+                    .disableAutocorrection(true)
+                    .textFieldStyle(.roundedBorder).padding()
+                
+                Text("Email").foregroundColor(.white)
+                TextField("Use Email valid ", text: $email)
+                    .disableAutocorrection(true)
+                    .textFieldStyle(.roundedBorder).padding()
+                
+                Text("Password").foregroundColor(.white)
+                .font(.title3)
+                SecureField("Use Password valid", text: $password)
+                    .textFieldStyle(.roundedBorder).padding()
+                
+                Text("Confirm Password").foregroundColor(.white)
+                .font(.title3)
+                SecureField("Use Password valid", text: $confirmPassword)
+                .textFieldStyle(.roundedBorder).padding()
+                
+                }
+            
+            Button("Create") {
+                
+            }
+           
+        }
     }
 }
 
