cff1684d7666bfb732dc3a600ebe936dac1bf9f2
Modibo KEITA <modykeita2@gmail.com>
2024-05-15 17:55:32 +0200

    update Candidats view with two buttons

--- Vitesse/Vitesse/View/Candidats.swift
+++ Vitesse/Vitesse/View/Candidats.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct Candidats: View {
     var body: some View {
-        Text("Candidats")
-        Image(systemName: "moon")
+     
+            ZStack {
+                Color.blue.opacity(0.5).ignoresSafeArea()
+                VStack{
+                    HStack {
+                        Button("Cancel") {
+                            
+                        }.frame(width: 100,height: 50)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                        
+                        Text("Candidats")  .font(.largeTitle)
+                            .fontWeight(.bold)
+                        .foregroundColor(.blue).padding()
+                        
+                        Button("Delete") {
+                            <#code#>
+                        }.frame(width: 100,height: 50)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                
+            }
+        }
     }
 }
 
