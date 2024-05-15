41d5ec5ec72ae5c871486350a300b1951611b9ac
Modibo KEITA <modykeita2@gmail.com>
2024-05-15 17:39:42 +0200

    update view Login

--- Vitesse/Vitesse/View/Login.swift
+++ Vitesse/Vitesse/View/Login.swift
@@ -10,50 +10,59 @@ import SwiftUI
 struct Login: View {
     
     @ObservedObject var loginViewModel : LoginViewModel
+    @State private var registre : Bool = false
     
     var body: some View {
-        ZStack {
-            Color.blue.opacity(0.5).ignoresSafeArea()
-            VStack {
-     
+        NavigationStack{
+            ZStack {
+                Color.blue.opacity(0.5).ignoresSafeArea()
+                VStack {
+                    
                     Text("Login")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.blue).padding()
-                
-                VStack{
-                    Text("Email/Username").foregroundColor(.white)
-                    TextField("Entrez un Email ou Username valide", text: $loginViewModel.username).disableAutocorrection(true)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue).padding()
+                    
+                    VStack{
+                        Text("Email/Username").foregroundColor(.white)
+                        TextField("Entrez un Email ou Username valide", text: $loginViewModel.username).disableAutocorrection(true)
                             .textFieldStyle(.roundedBorder)
                     }
-                VStack{
+                    VStack{
                         Text("Password").foregroundColor(.white)
-                        .font(.title3)
-                    SecureField("Veuillez entrez un mot de passe vaide", text: $loginViewModel.password)
-                        .textFieldStyle(.roundedBorder)
+                            .font(.title3)
+                        SecureField("Veuillez entrez un mot de passe vaide", text: $loginViewModel.password)
+                            .textFieldStyle(.roundedBorder)
                         
-                }.padding()
-                
-                Button("Sign in") {
-                    Task{@MainActor in
-                        try? await loginViewModel.authentification()
-
+                    }.padding()
+                    
+                    Button("Sign in") {
+                        Task{@MainActor in
+                            try? await loginViewModel.authentification()
+                            
+                        }
+                        
+                    }.frame(width: 100,height: 50).foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(10)
+                    
+                    NavigationLink(destination: Registre(), isActive: $registre) {
+                        EmptyView()
                     }
-                    
-                }.frame(width: 100,height: 50).foregroundColor(.white)
-                    .background(.blue)
-                    .cornerRadius(10)
                 
-                Button("Registrer") {
-                    
-                }.frame(width: 100,height: 50)
-                    .background(.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                    Button("Registrer") {
+                        
+                        registre = true
 
-                
+                    }.frame(width: 100,height: 50)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    
+                    
+                }
+                .padding()
             }
-            .padding()
         }
     }
 }
