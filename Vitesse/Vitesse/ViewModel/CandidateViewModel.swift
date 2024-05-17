3ede08104a605c089987e3dbc2be5bee960c84a3
Modibo KEITA <modykeita2@gmail.com>
2024-05-17 20:03:02 +0200

    update CandidateViewModel.swift

--- Vitesse/Vitesse/ViewModel/CandidateViewModel.swift
+++ Vitesse/Vitesse/ViewModel/CandidateViewModel.swift
@@ -8,29 +8,14 @@
 import Foundation
 
 class CandidateViewModel : ObservableObject{
-    @Published var phone : JSONNull? = nil
-    @Published var note : JSONNull? = nil
-    @Published var id : String = ""
-    @Published var firstName : String = ""
-    @Published var linkedinURL: JSONNull? = nil
-    @Published var isFavorite: Bool
-    @Published var email = ""
-    @Published var lastName: String = ""
-    
+   
     let candidateProfile : CandidateProfile
     let keychain = Keychain()
 
     
     
-    init(phone: JSONNull? = nil, note: JSONNull? = nil, id: String, firstName: String, linkedinURL: JSONNull? = nil, isFavorite: Bool, email: String = "", lastName: String,candidateProfile : CandidateProfile) {
-        self.phone = phone
-        self.note = note
-        self.id = id
-        self.firstName = firstName
-        self.linkedinURL = linkedinURL
-        self.isFavorite = isFavorite
-        self.email = email
-        self.lastName = lastName
+    init(candidateProfile : CandidateProfile) {
+        
         self.candidateProfile = candidateProfile
     }
     
