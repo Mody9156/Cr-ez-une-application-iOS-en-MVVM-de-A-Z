974c9e3f0a7b3f8dff68798f51c33b8fff4a3cb3
Modibo KEITA <modykeita2@gmail.com>
2024-05-22 12:28:58 +0200

    reinitialise struc

--- Vitesse/Vitesse/View/CandidateDetailsView.swift
+++ Vitesse/Vitesse/View/CandidateDetailsView.swift
@@ -2,36 +2,26 @@ import SwiftUI
 
 struct CandidateDetailView: View {
     @StateObject var candidateViewModel: CandidateViewModel
-    let candidate: RecruitTech
-    @State private var isLoading = false
+
 
     var body: some View {
         VStack {
-            if isLoading {
-                ProgressView("Loading...")
-            } else if let info = candidate {
-                Text(info.lastName)
-                Text(info.firstName)
-                
-            } else {
-                Text("No details available.")
+            
+            
+        }.onAppear{
+            Task{
+              await  candidat()
             }
         }
-        .onAppear {
-            loadCandidateDetails()
+    }
+    
+    func candidat() async {
+        do{
+            let data = try await candidateViewModel.fetchcandidateIDFetcher()
+            print("felicitation")
+        }catch{
+            print("dommage  candidat n'est pas passé ")
         }
     }
 
-    func loadCandidateDetails() {
-        Task {
-            do {
-                isLoading = true
-                let details = try await candidateViewModel.fetchDetails(for: candidate.id)
-                detailedInfo = details
-            } catch {
-                print("Failed to load candidate details: \(error)")
-            }
-            isLoading = false
-        }
-    }
 }
