b06bc062972a5892ebf7a6f81e2cdd36a671fc78
Modibo KEITA <modykeita2@gmail.com>
2024-05-23 20:41:56 +0200

    update function

--- Vitesse/Vitesse/ViewModel/Candidate/CandidateDetailsManagerVieModel.swift
+++ Vitesse/Vitesse/ViewModel/Candidate/CandidateDetailsManagerVieModel.swift
@@ -16,7 +16,7 @@ class CandidateDetailsManager : ObservableObject {
         self.retrieveCandidateData = retrieveCandidateData
     }
     enum FetchTokenResult: Error, LocalizedError {
-        case displayCandidateDetailsError,fetchTokenError
+        case displayCandidateDetailsError,fetchTokenError,candidateUpdaterError
     }
     
     @MainActor
@@ -43,4 +43,21 @@ class CandidateDetailsManager : ObservableObject {
             throw FetchTokenResult.displayCandidateDetailsError
         }
     }
+    
+    func candidateUpdater(at offsets: IndexSet,phone:String?,note:String?,firstName:String,linkedinURL: String?,isFavorite: Bool,email:String,lastName: String) async throws -> [CandidateInformation]  {
+        do {
+            let getToken = try await getToken()
+            var id = ""
+            for offset in offsets {
+                id = candidats[offset].id
+            }
+            let request = try CandidateManagement.createURLRequestfornewcandidat(url: "http://127.0.0.1:8080/candidate/\(id)", method: "PUT", token: getToken, id: id, phone: phone, note: note, firstName: firstName, linkedinURL: linkedinURL, isFavorite: isFavorite, email: email, lastName: lastName)
+            let data = try await retrieveCandidateData.accessCandidateCreationRequest(token: getToken, id: id, phone: phone, note: note, firstName: firstName, linkedinURL: linkedinURL, isFavorite: isFavorite, email: email, lastName: lastName, request: request)
+            return data
+        }catch{
+            throw FetchTokenResult.candidateUpdaterError
+        }
+    }
+    
+    
 }
