424bdb8da052f41a71a31b4755fd1e195e8bff64
Modibo KEITA <modykeita2@gmail.com>
2024-05-24 12:36:26 +0200

    add group

--- Vitesse/Vitesse/View/CandidateDetailsView.swift
+++ Vitesse/Vitesse/View/CandidateDetailsView.swift
@@ -3,58 +3,59 @@ struct CandidateDetailView: View {
     @ObservedObject var candidateDetailsManager: CandidateDetailsManager
      var candidate: CandidateInformation // Assurez-vous que CandidateInformation est le bon type
     var body: some View {
-        ZStack {
-            Color.blue.opacity(0.5).ignoresSafeArea()
-           
-                        VStack {
-                            HStack {
-                                Text(candidate.lastName)
-                                Text(candidate.firstName)
-                                Spacer()
-                                Image(systemName: candidate.isFavorite ? "star.fill" : "star")
-                                    .foregroundColor(candidate.isFavorite ? .yellow : .black)
-                            }
-                            HStack {
-                                Text("Phone")
-                                if let phone = candidate.phone {
-                                    Text(phone)
-                                } else {
-                                    Text("No phone available")
-                                        .foregroundColor(.gray)
-                                }
-                            }
-                            HStack {
-                                Text("Email")
-                                Text(candidate.email)
-                            }
-                            HStack {
-                                Text("LinkedIn")
-                                if let linkedIn = candidate.linkedinURL {
-                                    Text(linkedIn)
-                                } else {
-                                    Text("No LinkedIn available")
-                                        .foregroundColor(.gray)
-                                }
-                            }
-                            Text("Note")
-                            if let note = candidate.note {
-                                Text(note)
-                            } else {
-                                Text("No note available")
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                        .padding()
-                    
-                    .onAppear {
-                        Task {
-                            await loadCandidateProfile()
-                        }
-                    }
-                .padding()
+        
+        VStack {
+            Group {
+
+            HStack {
+                Text(candidate.lastName)
+                Text(candidate.firstName)
+                Spacer()
+                Image(systemName: candidate.isFavorite ? "star.fill" : "star")
+                    .foregroundColor(candidate.isFavorite ? .yellow : .black)
+            }
+            HStack {
+                Text("Phone")
+                if let phone = candidate.phone {
+                    Text(phone)
+                } else {
+                    Text("No phone available")
+                        .foregroundColor(.gray)
+                }
+            }
+            HStack {
+                Text("Email")
+                Text(candidate.email)
+            }
+            HStack {
+                Text("LinkedIn")
+                if let linkedIn = candidate.linkedinURL {
+                    Text(linkedIn)
+                } else {
+                    Text("No LinkedIn available")
+                        .foregroundColor(.gray)
+                }
+            }
+            }
+            Text("Note")
+            if let note = candidate.note {
+                Text(note)
+            } else {
+                Text("No note available")
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding()
+        
+        .onAppear {
+            Task {
+                await loadCandidateProfile()
+            }
+        }
+  
+                
                 }
             
-    }
 
     func loadCandidateProfile() async {
         do {
