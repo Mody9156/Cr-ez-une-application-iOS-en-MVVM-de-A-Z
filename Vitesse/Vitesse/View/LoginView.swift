c06cba0e0355f1b05d1e848460e7ff1f1830ed33
Modibo KEITA <modykeita2@gmail.com>
2024-05-28 11:16:34 +0200

    update resizable

--- Vitesse/Vitesse/View/LoginView.swift
+++ Vitesse/Vitesse/View/LoginView.swift
@@ -22,7 +22,7 @@ struct LoginView: View {
                         .fontWeight(.bold)
                         .foregroundColor(.blue)
                         .padding()
-                    Image("running_").frame(width: 50,height: 50)
+                    Image("running_").resizable()
                     VStack {
                         Text("Email/Username")
                             .foregroundColor(.white)
