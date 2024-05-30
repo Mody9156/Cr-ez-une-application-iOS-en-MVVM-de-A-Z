fd9ff151f98f965f224a2f7cdfdbacd89152aa77
Modibo KEITA <modykeita2@gmail.com>
2024-05-30 16:47:16 +0200

    replace || par &&

--- Vitesse/Vitesse/View/CandidatesListView.swift
+++ Vitesse/Vitesse/View/CandidatesListView.swift
@@ -57,7 +57,7 @@ struct CandidatesListView: View {
             return candidateListViewModel.candidates
         } else {
             return candidateListViewModel.candidates.filter { candidat in
-                candidat.lastName.lowercased().contains(search.lowercased()) ||
+                candidat.lastName.lowercased().contains(search.lowercased()) &&
                 candidat.firstName.lowercased().contains(search.lowercased())
             }
         }
