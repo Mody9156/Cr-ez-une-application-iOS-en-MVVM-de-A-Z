85570be026a120f2f28983f64101ebdcb9159682
Modibo KEITA <modykeita2@gmail.com>
2024-05-31 17:42:14 +0200

    custu

--- Vitesse/Vitesse/View/CandidatesListView.swift
+++ Vitesse/Vitesse/View/CandidatesListView.swift
@@ -1,29 +1,31 @@
 import SwiftUI
+
 struct CandidatesListView: View {
     @StateObject var candidateListViewModel: CandidateListViewModel
     @State private var search = ""
-    @State private var showFavorites : Bool = false
-    
+    @State private var showFavorites: Bool = false
+
     var body: some View {
         NavigationView {
             ZStack {
+                Color.orange.opacity(0.1)
+                    .edgesIgnoringSafeArea(.all)
+                
                 VStack {
-              
                     List {
                         ForEach(searchResult, id: \.id) { candidate in
                             if !showFavorites || candidate.isFavorite {
-                                NavigationLink(destination: CandidateDetailView(CandidateDetailsManagerViewModel: CandidateDetailsManagerViewModel(retrieveCandidateData: CandidateDataManager(), candidats: candidateListViewModel.candidates), CandidateInformation: candidate)){
+                                NavigationLink(destination: CandidateDetailView(CandidateDetailsManagerViewModel: CandidateDetailsManagerViewModel(retrieveCandidateData: CandidateDataManager(), candidats: candidateListViewModel.candidates), CandidateInformation: candidate)) {
                                     HStack {
-                                        Text(candidate.lastName).foregroundColor(.black)
-                                        Text(candidate.firstName).foregroundColor(.black)
+                                        Text(candidate.lastName).foregroundColor(.orange)
+                                        Text(candidate.firstName).foregroundColor(.orange)
                                         Spacer()
                                         
-                                        Image(systemName:candidate.isFavorite ? "star.fill" :"star")
-                                            .foregroundColor(candidate.isFavorite ? .yellow : .black)
-                                            
+                                        Image(systemName: candidate.isFavorite ? "star.fill" : "star")
+                                            .foregroundColor(candidate.isFavorite ? .yellow : .gray)
                                     }
-                                       
-                                }.listRowSeparatorTint(.orange)
+                                }
+                                .listRowSeparatorTint(.orange)
                             }
                         }
                         .onDelete(perform: candidateListViewModel.removeCandidate)
@@ -31,8 +33,8 @@ struct CandidatesListView: View {
                     .toolbar {
                         ToolbarItem(placement: .navigationBarLeading) {
                             EditButton()
-                                .frame(width: 40,height: 40)
-                                .foregroundColor(.blue)
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.orange)
                         }
                         ToolbarItem(placement: .navigationBarTrailing) {
                             Button {
@@ -45,35 +47,34 @@ struct CandidatesListView: View {
                                         print("Dommage, il y a une erreur :", error)
                                     }
                                 }
-
                             } label: {
-                                Image(systemName: showFavorites ?  "star.fill":"star").foregroundColor(showFavorites ? .yellow : .black)
+                                Image(systemName: showFavorites ? "star.fill" : "star")
+                                    .foregroundColor(showFavorites ? .yellow : .gray)
                             }
-                            .frame(width: 40,height: 40)
-                            .foregroundColor(.blue)
+                            .frame(width: 40, height: 40)
                         }
-                       
                     }
                     .searchable(text: $search)
                 }
+                .padding()
             }
         }
         .task {
             await loadCandidates()
         }
     }
-    
+
     var searchResult: [CandidateInformation] {
         if search.isEmpty {
             return candidateListViewModel.candidates
         } else {
             return candidateListViewModel.candidates.filter { candidat in
                 candidat.lastName.lowercased().contains(search.lowercased()) ||
-                candidat.firstName.lowercased().contains(search.lowercased()) 
+                candidat.firstName.lowercased().contains(search.lowercased())
             }
         }
     }
-    
+
     func loadCandidates() async {
         do {
             let candidats = try await candidateListViewModel.displayCandidatesList()
@@ -82,6 +83,4 @@ struct CandidatesListView: View {
             print("Erreur lors de la récupération des candidats")
         }
     }
-    
-  
 }
