ba9123b2d685ee161ed2ebe1fb32e3dcc5583206
Modibo KEITA <modykeita2@gmail.com>
2024-06-03 12:22:22 +0200

    updte list

--- Vitesse/Vitesse/View/CandidatesListView.swift
+++ Vitesse/Vitesse/View/CandidatesListView.swift
@@ -6,69 +6,71 @@ struct CandidatesListView: View {
     @State private var showFavorites: Bool = false
 
     var body: some View {
-        
-        VStack {
-            NavigationStack {
-                    // List for the candidates
-                    List {
-                        ForEach(searchResult, id: \.id) { candidate in
-                            if !showFavorites || candidate.isFavorite {
-                                NavigationLink(destination: CandidateDetailView(CandidateDetailsManagerViewModel: CandidateDetailsManagerViewModel(retrieveCandidateData: CandidateDataManager(), candidats: candidateListViewModel.candidates), CandidateInformation: candidate)) {
-                                    HStack {
-                                        Text(candidate.lastName).foregroundColor(.orange)
-                                        Text(candidate.firstName).foregroundColor(.orange)
-                                        Spacer()
-                                        Image(systemName: candidate.isFavorite ? "star.fill" : "star")
-                                            .foregroundColor(candidate.isFavorite ? .yellow : .black)
-                                    }
-                                
-                                }.listRowSeparator(.visible,edges: .bottom)
-                                .listRowSeparator(.visible, edges: .top)
-                            }
-                        }
-                        .onDelete(perform: candidateListViewModel.removeCandidate)
-                    }
-
-                    // Toolbar at the bottom
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            EditButton()
-                                .frame(width: 40, height: 40)
-                                .foregroundColor(.orange)
-                        }
-                        
-                        ToolbarItem(placement: .navigation) {
-                            Text("Candidats")
-                        }
-                      
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            Button {
-                                Task {
-                                    do {
-                                        let candidate = try await candidateListViewModel.showFavoriteCandidates()
-                                        print("La mise à jour du statut du favori pour le candidat a réussi. : \(String(describing: candidate))")
-                                        self.showFavorites.toggle()
-                                    } catch {
-                                        print("Dommage, il y a une erreur :", error)
-                                    }
+        NavigationView {
+            VStack {
+                // Liste des candidats
+                List {
+                    ForEach(searchResult, id: \.id) { candidate in
+                        if !showFavorites || candidate.isFavorite {
+                            NavigationLink(
+                                destination: CandidateDetailView(
+                                    CandidateDetailsManagerViewModel: CandidateDetailsManagerViewModel(
+                                        retrieveCandidateData: CandidateDataManager(),
+                                        candidats: candidateListViewModel.candidates
+                                    ),
+                                    CandidateInformation: candidate
+                                )
+                            ) {
+                                HStack {
+                                    Text(candidate.lastName)
+                                        .foregroundColor(.orange)
+                                    Text(candidate.firstName)
+                                        .foregroundColor(.orange)
+                                    Spacer()
+                                    Image(systemName: candidate.isFavorite ? "star.fill" : "star")
+                                        .foregroundColor(candidate.isFavorite ? .yellow : .black)
                                 }
-                            } label: {
-                                Image(systemName: showFavorites ? "star.fill" : "star")
-                                    .foregroundColor(showFavorites ? .yellow : .black)
                             }
-                            .frame(width: 40, height: 40)
+                            .listRowSeparator(.hidden)
+                            .listRowBackground(Color.clear)
+                            .listRowSeparator(.orange)
                         }
-                    }.searchable(text: $search)
+                    }
+                    .onDelete(perform: candidateListViewModel.removeCandidate)
                 }
-                .padding()
-                
-                
+                .listStyle(PlainListStyle()) // Utiliser un style de liste simple
+                .background(Color.white) // Ajouter une couleur d'arrière-plan à la liste
+                .searchable(text: $search)
+                .toolbar {
+                    // Bouton pour modifier la liste
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EditButton()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.orange)
+                    }
+                    // Titre de la vue
+                    ToolbarItem(placement: .navigation) {
+                        Text("Candidats")
+                    }
+                    // Bouton pour afficher/masquer les favoris
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: toggleShowFavorites) {
+                            Image(systemName: showFavorites ? "star.fill" : "star")
+                                .foregroundColor(showFavorites ? .yellow : .black)
+                        }
+                        .frame(width: 40, height: 40)
+                    }
+                }
+            }
+            .padding()
+            .background(Color.white) // Ajouter une couleur d'arrière-plan à la vue globale
             .task {
                 await loadCandidates()
-        }
+            }
         }
     }
 
+    // Résultats de la recherche
     var searchResult: [CandidateInformation] {
         if search.isEmpty {
             return candidateListViewModel.candidates
@@ -80,12 +82,26 @@ struct CandidatesListView: View {
         }
     }
 
+    // Charger les candidats
     func loadCandidates() async {
         do {
             let candidats = try await candidateListViewModel.displayCandidatesList()
             candidateListViewModel.candidates = candidats
         } catch {
             print("Erreur lors de la récupération des candidats")
+        }
+    }
+
+    // Basculer l'affichage des favoris
+    func toggleShowFavorites() {
+        Task {
+            do {
+                let candidate = try await candidateListViewModel.showFavoriteCandidates()
+                print("La mise à jour du statut du favori pour le candidat a réussi : \(String(describing: candidate))")
+                showFavorites.toggle()
+            } catch {
+                print("Dommage, il y a une erreur :", error)
+            }
         }
     }
 }
