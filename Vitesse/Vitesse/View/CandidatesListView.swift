6dd7aa93ef07f3f34f8fa12c0d55e85484e999a3
Modibo KEITA <modykeita2@gmail.com>
2024-06-03 15:51:58 +0200

    update foreach

--- Vitesse/Vitesse/View/CandidatesListView.swift
+++ Vitesse/Vitesse/View/CandidatesListView.swift
@@ -11,55 +11,51 @@ struct CandidatesListView: View {
                 // Liste des candidats
                 List {
                     ForEach(searchResult, id: \.id) { candidate in
-                        if !showFavorites || candidate.isFavorite {
-                            NavigationLink(
-                                destination: CandidateDetailView(
-                                    CandidateDetailsManagerViewModel: CandidateDetailsManagerViewModel(
-                                        retrieveCandidateData: CandidateDataManager(),
-                                        candidats: candidateListViewModel.candidates
-                                    ),
-                                    CandidateInformation: candidate
-                                )
-                            ) {
-                                HStack {
-                                    Text(candidate.lastName)
-                                        .foregroundColor(.orange)
-                                    Text(candidate.firstName)
-                                        .foregroundColor(.orange)
-                                    Spacer()
-                                    Image(systemName: candidate.isFavorite ? "star.fill" : "star")
-                                        .foregroundColor(candidate.isFavorite ? .yellow : .black)
-                                }
+                        NavigationLink(
+                            destination: CandidateDetailView(
+                                CandidateDetailsManagerViewModel: CandidateDetailsManagerViewModel(
+                                    retrieveCandidateData: CandidateDataManager(),
+                                    candidats: candidateListViewModel.candidates
+                                ),
+                                CandidateInformation: candidate
+                            )
+                        ) {
+                            HStack {
+                                Text(candidate.firstName)
+                                    .foregroundColor(.orange)
+                                Text(candidate.lastName)
+                                    .foregroundColor(.orange)
+                               
+                                Spacer()
+                                Image(systemName: candidate.isFavorite ? "star.fill" : "star")
+                                    .foregroundColor(candidate.isFavorite ? .yellow : .black)
                             }
-                            .listRowSeparator(.visible)
-                            .listRowBackground(Color.clear)
-                            .listSectionSeparatorTint(.orange)
                         }
+                        .listRowSeparator(.visible)
+                        .listRowBackground(Color.clear)
+                        .listSectionSeparatorTint(.orange)
                     }
                     .onDelete(perform: candidateListViewModel.removeCandidate)
                 }
-                .listStyle(PlainListStyle()) // Utiliser un style de liste simple
-                .background(Color.white) // Ajouter une couleur d'arrière-plan à la liste
+                .listStyle(PlainListStyle())
+                .background(Color.white)
                 .searchable(text: $search)
                 .toolbar {
-                    // Bouton pour modifier la liste
                     ToolbarItem(placement: .navigationBarLeading) {
                         EditButton()
-                            .frame(width: 40, height: 40).foregroundColor(.orange)
-                           
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.orange)
                     }
-                    // Titre de la vue
                     ToolbarItem(placement: .principal) {
-                                           HStack {
-                                               Spacer()
-                                               Text("Candidats")
-                                                   .font(.headline)
-                                                   .foregroundColor(.orange)
-                                               Spacer()
-                                           }
-                                           .frame(maxWidth: .infinity)
-                                       }
-                    // Bouton pour afficher/masquer les favoris
+                        HStack {
+                            Spacer()
+                            Text("Candidats")
+                                .font(.headline)
+                                .foregroundColor(.orange)
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
                     ToolbarItem(placement: .navigationBarTrailing) {
                         Button(action: toggleShowFavorites) {
                             Image(systemName: showFavorites ? "star.fill" : "star")
@@ -69,6 +65,8 @@ struct CandidatesListView: View {
                     }
                 }
             }
+            .padding()
+            .background(Color.white)
             .task {
                 await loadCandidates()
             }
@@ -78,11 +76,14 @@ struct CandidatesListView: View {
     // Résultats de la recherche
     var searchResult: [CandidateInformation] {
         if search.isEmpty {
-            return candidateListViewModel.candidates
+            return candidateListViewModel.candidates.filter { candidate in
+                !showFavorites || candidate.isFavorite
+            }
         } else {
-            return candidateListViewModel.candidates.filter { candidat in
-                candidat.lastName.lowercased().contains(search.lowercased()) ||
-                candidat.firstName.lowercased().contains(search.lowercased())
+            return candidateListViewModel.candidates.filter { candidate in
+                (candidate.lastName.lowercased().contains(search.lowercased()) ||
+                candidate.firstName.lowercased().contains(search.lowercased())) &&
+                (!showFavorites || candidate.isFavorite)
             }
         }
     }
@@ -99,14 +100,6 @@ struct CandidatesListView: View {
 
     // Basculer l'affichage des favoris
     func toggleShowFavorites() {
-        Task {
-            do {
-                let candidate = try await candidateListViewModel.showFavoriteCandidates()
-                print("La mise à jour du statut du favori pour le candidat a réussi : \(String(describing: candidate))")
-                showFavorites.toggle()
-            } catch {
-                print("Dommage, il y a une erreur :", error)
-            }
-        }
+        showFavorites.toggle()
     }
 }
