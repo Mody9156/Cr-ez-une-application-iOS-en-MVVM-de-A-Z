bc71c5c160f3c06b328d39348c8b00275410d8e2
Modibo KEITA <modykeita2@gmail.com>
2024-06-04 11:38:39 +0200

    refactor

--- Vitesse/Vitesse/View/RegistrationView.swift
+++ Vitesse/Vitesse/View/RegistrationView.swift
@@ -3,55 +3,56 @@ import SwiftUI
 struct RegistrationView: View {
     @State var registerViewModel = RegisterViewModel(registrationRequestBuilder: RegistrationRequestBuilder(httpService: URLSessionHTTPClient()), loginViewModel: LoginViewModel({}))
     @State private var registre: Bool = false
-    @State var infos : String = ""
-    @State var login : LoginViewModel
+    @State var infos: String = ""
+    @State var login: LoginViewModel
 
     var body: some View {
-            ZStack {
-                Color.orange.opacity(0.2) // Fond orange clair
-                    .ignoresSafeArea()
-                VStack {
-                    Text("Registre")
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.orange)
-                        .padding(.bottom,20)
+        ZStack {
+            Color.orange.opacity(0.2) // Light orange background
+                .ignoresSafeArea()
+            VStack {
+                Text("Register")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.orange)
+                    .padding(.bottom, 20)
 
-                  
-                    VStack(alignment: .leading) {
-                        FetchRegistre(registreViewModel: $registerViewModel, infos: "First Name",text:registerViewModel.firstName,textField: "Use First Name valid")
-                        FetchRegistre(registreViewModel: $registerViewModel, infos: "Last Name",text:registerViewModel.lastName,textField: "Use Last Name valid")
-                        FetchRegistre(registreViewModel: $registerViewModel, infos: "Email",text:registerViewModel.email,textField: "Use Email valid ")
+                VStack(alignment: .leading) {
+                    FetchRegister(registreViewModel: $registerViewModel, infos: "First Name", text: registerViewModel.firstName, textField: "Use a valid First Name")
+                    FetchRegister(registreViewModel: $registerViewModel, infos: "Last Name", text: registerViewModel.lastName, textField: "Use a valid Last Name")
+                    FetchRegister(registreViewModel: $registerViewModel, infos: "Email", text: registerViewModel.email, textField: "Use a valid Email")
 
-                        PasswordInputField(registreViewModel: $registerViewModel,textNames:"Password")
-                        PasswordInputField(registreViewModel: $registerViewModel, textNames:" Confirm Password")
-                    }
-                    .padding()
-
-                    Button("Create") {
-                        Task {
-                            try await registerViewModel.handleRegistrationViewModel()
-                            self.login.username = registerViewModel.email
-                            
-                        }
-                    }
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.orange)
-                    .cornerRadius(10)
+                    PasswordInputField(registreViewModel: $registerViewModel, textNames: "Password")
+                    PasswordInputField(registreViewModel: $registerViewModel, textNames: "Confirm Password")
                 }
                 .padding()
+
+                Button("Create") {
+                    Task {
+                        do {
+                            try await registerViewModel.handleRegistrationViewModel()
+                            self.login.username = registerViewModel.email
+                        } catch {
+                            print("Error while creating the account: \(error)")
+                        }
+                    }
+                }
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.orange)
+                .cornerRadius(10)
             }
-        
+            .padding()
+        }
     }
 }
 
-struct FetchRegistre: View {
-    @Binding var registreViewModel : RegisterViewModel
-    @State var infos : String = ""
-    @State var text : String = ""
-    @State var textField : String = ""
+struct FetchRegister: View {
+    @Binding var registreViewModel: RegisterViewModel
+    @State var infos: String = ""
+    @State var text: String = ""
+    @State var textField: String = ""
     
     var body: some View {
         Group {
@@ -64,28 +65,17 @@ struct FetchRegistre: View {
                     RoundedRectangle(cornerRadius: 5)
                         .stroke(Color.orange, lineWidth: 2)
                 )
-
         }
     }
 }
 
 struct PasswordInputField: View {
-    @Binding var registreViewModel : RegisterViewModel
-    var textNames : String = ""
+    @Binding var registreViewModel: RegisterViewModel
+    var textNames: String = ""
     
     var body: some View {
         Group {
             Text(textNames).foregroundColor(.orange)
-                .font(.title3)
-            SecureField("Use Password valid", text: $registreViewModel.password)
-                .padding()
-                .cornerRadius(5.0)
-                .foregroundColor(.black)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.orange, lineWidth: 2)
-                )
-            
         }
     }
 }
--- Vitesse/Vitesse/ViewModel/Registre/RegisterViewModel.swift
+++ Vitesse/Vitesse/ViewModel/Registre/RegisterViewModel.swift
@@ -1,35 +1,30 @@
-//
-//  RegistreViewModel.swift
-//  Vitesse
-//
-//  Created by KEITA on 16/05/2024.
-//
-
 import Foundation
 
-class RegisterViewModel : ObservableObject {
-    @Published var email : String = "Andrew@gmail.com"
-    @Published var password : String = "Modibokeita6582"
+class RegisterViewModel: ObservableObject {
+    @Published var email: String = "Andrew@gmail.com"
+    @Published var password: String = "Modibokeita6582"
     @Published var firstName: String = "Andrew"
     @Published var lastName: String = "Kyrie"
-    let loginViewModel : LoginViewModel
-  
-    let registrationRequestBuilder : RegistrationRequestBuilder
+    let loginViewModel: LoginViewModel
+    let registrationRequestBuilder: RegistrationRequestBuilder
     
-    init(registrationRequestBuilder: RegistrationRequestBuilder,loginViewModel : LoginViewModel) {
+    init(registrationRequestBuilder: RegistrationRequestBuilder, loginViewModel: LoginViewModel) {
         self.registrationRequestBuilder = registrationRequestBuilder
         self.loginViewModel = loginViewModel
     }
     
-    
-    func handleRegistrationViewModel() async throws   {
-        do{
-            let buildRegistrationRequest = try await registrationRequestBuilder.buildRegistrationRequest(email: email, password: password, firstName: firstName, lastName: lastName)
-                print("Vous venez d'enregistrer : \(buildRegistrationRequest)")
+    func handleRegistrationViewModel() async throws {
+        do {
+            let buildRegistrationRequest = try await registrationRequestBuilder.buildRegistrationRequest(
+                email: email,
+                password: password,
+                firstName: firstName,
+                lastName: lastName)
+            
+            print("You have just registered: \(buildRegistrationRequest)")
             loginViewModel.username = email
-        }catch{
-          throw  error
+        } catch {
+            throw error
         }
     }
-    
 }
