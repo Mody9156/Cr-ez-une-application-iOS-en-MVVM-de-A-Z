8ef850abbd480abfd446b34a36915fc451ecee05
Modibo KEITA <modykeita2@gmail.com>
2024-06-04 15:43:47 +0200

    update view

--- Vitesse/Vitesse/View/RegistrationView.swift
+++ Vitesse/Vitesse/View/RegistrationView.swift
@@ -17,13 +17,13 @@ struct RegistrationView: View {
                     .padding(.bottom, 20)
 
                 VStack(alignment: .leading) {
-                    FetchRegister( infos: "First Name", text: registerViewModel.firstName, textField: "Use a valid First Name")
-                    FetchRegister( infos: "Last Name", text: registerViewModel.lastName, textField: "Use a valid Last Name")
+                    FetchRegister( infos: "First Name", text: registerViewModel.firstName, textField: "Enter your last name")
+                    FetchRegister( infos: "Last Name", text: registerViewModel.lastName, textField: "Enter your name")
                     
                     FetchRegister(infos: "Email", text: registerViewModel.email, textField: "Use a valid Email").keyboardType(.emailAddress)
 
                     PasswordInputField( textField:"Enter your password", text:registerViewModel.password, textNames: "Password")
-                    PasswordInputField( textField:"Use a valid Password", text:registerViewModel.password, textNames: "Confirm Password")
+                    PasswordInputField( textField:"Enter your password", text:registerViewModel.password, textNames: "Confirm Password")
                 }
                 .padding()
 
