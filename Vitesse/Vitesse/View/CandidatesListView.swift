c0d06086932def8fd4b1a3d09c14b33e2b222860
Modibo KEITA <modykeita2@gmail.com>
2024-06-06 19:14:53 +0200

    deplace searchbar

--- Vitesse/Vitesse/View/CandidatesListView.swift
+++ Vitesse/Vitesse/View/CandidatesListView.swift
@@ -9,7 +9,6 @@ struct CandidatesListView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                
                 // Candidates list
                 List {
                     ForEach(searchResult, id: \.id) { candidate in
@@ -38,6 +37,11 @@ struct CandidatesListView: View {
                 }.searchable(text: $search)
                 .listStyle(PlainListStyle())
                 .background(Color.white)
+                
+                .toolbar {
+                    toolbarContent
+                }
+                
             }
             .padding()
             .background(Color.white)
@@ -66,6 +70,7 @@ struct CandidatesListView: View {
         }
     }
 
+
     // Load candidates
     func loadCandidates() async {
         do {
@@ -73,6 +78,39 @@ struct CandidatesListView: View {
             candidateListViewModel.candidats = candidates
         } catch {
             print("Error loading candidates")
+        }
+    }
+    
+    // Toggle favorites view
+    func toggleShowFavorites() {
+        showFavorites.toggle()
+    }
+    
+    // Toolbar content
+    @ToolbarContentBuilder
+    var toolbarContent: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) {
+            EditButton()
+                .frame(width: 40, height: 40)
+                .foregroundColor(.orange)
+        }
+        ToolbarItem(placement: .principal) {
+            HStack {
+                Spacer()
+                Text("Candidates")
+                    .font(.headline)
+                    .foregroundColor(.orange)
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+        }
+        
+        ToolbarItem(placement: .navigationBarTrailing) {
+            Button(action: toggleShowFavorites) {
+                Image(systemName: showFavorites ? "star.fill" : "star")
+                    .foregroundColor(showFavorites ? .yellow : .orange)
+            }
+            .frame(width: 40, height: 40)
         }
     }
 }
