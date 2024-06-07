2d2a5a3aa40d2528f66dd4b853a0a6ef4c7faa89
Modibo KEITA <modykeita2@gmail.com>
2024-06-07 11:22:34 +0200

    add comment

--- Vitesse/Vitesse/View/CandidateDetailsView.swift
+++ Vitesse/Vitesse/View/CandidateDetailsView.swift
@@ -14,107 +14,107 @@ struct CandidateDetailView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Section {
-                HStack {
-                    if isEditing {
-                        TextFieldManager(textField: "First Name", text: editedFirstName)
-                        TextFieldManager(textField: "Last Name", text: editedLastName)
-                          
-                    } else {
-                        Text(CandidateInformation.firstName)
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                        Text(CandidateInformation.lastName)
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                    }
-                    Spacer()
-                    if CandidateInformation.isFavorite {
-                        Image(systemName: "star.fill")
-                            .foregroundColor(.yellow)
-                            .font(.title2)
-                    }
-                }
-                .padding()
-
-                HStack {
-                    Text("Phone :")
-                    if isEditing {
-                        TextField("Phone", text: Binding(
-                            get: { editedPhone ?? "" },
-                            set: { editedPhone = $0 }
-                        ))
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    } else {
-                        if let phone = CandidateInformation.phone {
-                            Text(phone)
-                        } else {
-                            Text("No phone available")
-                                .foregroundColor(.gray)
-                        }
-                    }
-                }
-                .padding()
-
-                HStack {
-                    Text("Email :")
-                    if isEditing {
-    
-                        TextFieldManager(textField: "Email", text: editedEmail)
-                    } else {
-                        Text(CandidateInformation.email)
-                    }
-                }
-                .padding()
-
-                HStack {
-                    Text("LinkedIn :")
-                    if isEditing {
-                        TextField("LinkedIn URL", text: Binding(
-                            get: { editedLinkedIn ?? "" },
-                            set: { editedLinkedIn = $0 }
-                        ))
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    } else {
-                        if let linkedIn = CandidateInformation.linkedinURL,
-                            let url = URL(string: linkedIn) {
-                        Link("Go on LinkedIn", destination: url)
-                                .padding().border(.orange).foregroundStyle(.white)
-                                .background(Color.orange).cornerRadius(10)
-                        } else {
-                            Text("No LinkedIn available")
-                                .foregroundColor(.gray)
-                        }
-                    }
-                }
-                .padding()
-
-                VStack(alignment: .leading) {
-                    Text("Note :")
-                    if isEditing {
-                        TextField("Note", text:  Binding(get: {editedNote ?? ""}, set: {editedNote = $0}))
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                    } else {
-                        if let note = CandidateInformation.note {
-                            RoundedRectangle(cornerRadius: 10)
-                                .strokeBorder(Color.orange, lineWidth: 1)
-                                .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
-                                .frame(height: 100)
-                                .overlay(
-                                    Text(note)
-                                        .padding()
-                                        .foregroundColor(.black),
-                                    alignment: .center
-                                )
-
-                        } else {
-                            Text("No note available")
-                                .foregroundColor(.gray)
-                        }
-                    }
-                }
-                .padding()
-            }
+//            Section {
+//                HStack {
+//                    if isEditing {
+//                        TextFieldManager(textField: "First Name", text: editedFirstName)
+//                        TextFieldManager(textField: "Last Name", text: editedLastName)
+//
+//                    } else {
+//                        Text(CandidateInformation.firstName)
+//                            .font(.largeTitle)
+//                            .fontWeight(.bold)
+//                        Text(CandidateInformation.lastName)
+//                            .font(.largeTitle)
+//                            .fontWeight(.bold)
+//                    }
+//                    Spacer()
+//                    if CandidateInformation.isFavorite {
+//                        Image(systemName: "star.fill")
+//                            .foregroundColor(.yellow)
+//                            .font(.title2)
+//                    }
+//                }
+//                .padding()
+//
+//                HStack {
+//                    Text("Phone :")
+//                    if isEditing {
+//                        TextField("Phone", text: Binding(
+//                            get: { editedPhone ?? "" },
+//                            set: { editedPhone = $0 }
+//                        ))
+//                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                    } else {
+//                        if let phone = CandidateInformation.phone {
+//                            Text(phone)
+//                        } else {
+//                            Text("No phone available")
+//                                .foregroundColor(.gray)
+//                        }
+//                    }
+//                }
+//                .padding()
+//
+//                HStack {
+//                    Text("Email :")
+//                    if isEditing {
+//
+//                        TextFieldManager(textField: "Email", text: editedEmail)
+//                    } else {
+//                        Text(CandidateInformation.email)
+//                    }
+//                }
+//                .padding()
+//
+//                HStack {
+//                    Text("LinkedIn :")
+//                    if isEditing {
+//                        TextField("LinkedIn URL", text: Binding(
+//                            get: { editedLinkedIn ?? "" },
+//                            set: { editedLinkedIn = $0 }
+//                        ))
+//                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                    } else {
+//                        if let linkedIn = CandidateInformation.linkedinURL,
+//                            let url = URL(string: linkedIn) {
+//                        Link("Go on LinkedIn", destination: url)
+//                                .padding().border(.orange).foregroundStyle(.white)
+//                                .background(Color.orange).cornerRadius(10)
+//                        } else {
+//                            Text("No LinkedIn available")
+//                                .foregroundColor(.gray)
+//                        }
+//                    }
+//                }
+//                .padding()
+//
+//                VStack(alignment: .leading) {
+//                    Text("Note :")
+//                    if isEditing {
+//                        TextField("Note", text:  Binding(get: {editedNote ?? ""}, set: {editedNote = $0}))
+//                            .textFieldStyle(RoundedBorderTextFieldStyle())
+//                    } else {
+//                        if let note = CandidateInformation.note {
+//                            RoundedRectangle(cornerRadius: 10)
+//                                .strokeBorder(Color.orange, lineWidth: 1)
+//                                .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+//                                .frame(height: 100)
+//                                .overlay(
+//                                    Text(note)
+//                                        .padding()
+//                                        .foregroundColor(.black),
+//                                    alignment: .center
+//                                )
+//
+//                        } else {
+//                            Text("No note available")
+//                                .foregroundColor(.gray)
+//                        }
+//                    }
+//                }
+//                .padding()
+//            }
             Spacer()
         }.navigationBarBackButtonHidden()
         .onAppear {
