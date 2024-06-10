231cfd0858fa5ad0f2ef9e9b616911a3a4f8f869
Modibo KEITA <modykeita2@gmail.com>
2024-06-10 17:26:48 +0200

    tests unitaires

--- Vitesse/VitesseTests/Model/VitesseTests.swift
+++ Vitesse/VitesseTests/Model/VitesseTests.swift
@@ -6,30 +6,82 @@
 //
 
 import XCTest
+@testable import Vitesse
 
 final class LoginTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var authenticationManager : AuthenticationManager!
+    
+    override func setUp() {
+        super.setUp()
+        // Given
+        // Initialisation de votre AuthConnector avec un HTTPService fictif pour les tests
+        authenticationManager = AuthenticationManager(httpService: MockHTTPService())
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        // Nettoyage après chaque test si nécessaire
+        authenticationManager = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func testBuildAuthenticationRequest() throws {
+        
+        struct EncodingLogin :Encodable {
+        var email: String
+        var password: String
         }
+        let name = "Paul"
+        let password = "test"
+        var encodeAuth = EncodingLogin(email: name, password: password)
+        var encode = try? JSONEncoder().encode(encodeAuth)
+
+        let useExpectedURL = URL(string: "http://exemple/auh)")!
+        
+        var request = URLRequest(url: useExpectedURL)
+        request.httpBody = encode
+        
+        let buildAuthenticationRequest =  authenticationManager.buildAuthenticationRequest(username: name, password: password)
+        
+
+        XCTAssertEqual(buildAuthenticationRequest.httpMethod,"POST" )
+        XCTAssertNotNil(buildAuthenticationRequest.url)
+        XCTAssertEqual(buildAuthenticationRequest.httpBody,request.httpBody)
+        XCTAssertNotNil(buildAuthenticationRequest.allHTTPHeaderFields)
+
+    }
+
+    func testAuthenticate() async throws {
+        let name = "Paul"
+        let password = "test"
+        
+        let JSONResponseDecodingModel =
+        """
+                    "name" : "Paul",
+                    "password" = "test"
+        
+            
+        """.data(using: .utf8)
+        
+//        let json = try JSONDecoder().decode(JSONResponseDecodingModel.self, from: data)
+
+        
+        let buildAuthenticationRequest = try await authenticationManager.authenticate(username: name, password: password)
+        
+    }
+    
+    // Mock HTTPService utilisé pour simuler les réponses HTTP
+    class MockHTTPService : HTTPService {
+        
+        var mockResult: (Data, HTTPURLResponse)?
+
+        func request(_ request: URLRequest) async throws -> (Data, HTTPURLResponse) {
+            guard let result = mockResult else {
+                throw NSError(domain: "", code: 0, userInfo: nil)
+            }
+            return result
+        }
+      
     }
 
 }
