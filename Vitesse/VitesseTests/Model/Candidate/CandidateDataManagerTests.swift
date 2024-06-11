b0d6ebd01fd09005435a9da59b1c2e101c457215
Modibo KEITA <modykeita2@gmail.com>
2024-06-11 14:40:37 +0200

    created func testFetchCandidateData

--- Vitesse/VitesseTests/Model/Candidate/CandidateDataManagerTests.swift
+++ Vitesse/VitesseTests/Model/Candidate/CandidateDataManagerTests.swift
@@ -13,20 +13,57 @@ final class CandidateDataManagerTests: XCTestCase {
     
     override func setUp()  {
         super.setUp()
+        candidateDataManager = CandidateDataManager(httpService: MockHTTPService())
     }
 
     override func tearDown()  {
         super.tearDown()
+        candidateDataManager = nil
     }
 
-    func testExample() throws {
-       
+    func testFetchCandidateData() throws {
+        
+        let  CandidateDecode = """
+{
+             "phone" : "0122333344",
+             "note": "Développeur en Backend",
+             "id" : "vzbjkzjbinkjzbjkz4254",
+             "firstName": "William",
+             "linkedinURL": "https://www.linkedin.com/in/William-William-123456789/
+",
+             "isFavorite": true,
+             "email" : "William.Browm@gmail.com",
+             "lastName": "Browm"
+        }
+""".data(using: .utf8)!
+        
+        struct CandidateEncode: Identifiable, Decodable ,Hashable{
+            var phone, note: String?
+            var id, firstName: String
+            var linkedinURL: String?
+            var isFavorite: Bool
+            var email, lastName: String
+        }
+        
+        
+        
+        let decode = try? JSONDecoder().decode([CandidateEncode].self, from: CandidateDecode)
+        
+        
+        
     }
 
-    func testPerformanceExample() throws {
+    func testfetchCandidateDetail() throws {
       
     }
     
+    func testvalidateHTTPResponse() throws {
+        
+    }
+    
+    func testfetchCandidateInformation() throws {
+        
+    }
     // Mock HTTPService utilisé pour simuler les réponses HTTP
     class MockHTTPService: HTTPService {
           
