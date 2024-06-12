d728891684db70c2d813aff30750bf1bc1b5a243
Modibo KEITA <modykeita2@gmail.com>
2024-06-12 15:24:56 +0200

    add function tests

--- Vitesse/VitesseTests/Model/Candidate/CandidateManagementTests.swift
+++ Vitesse/VitesseTests/Model/Candidate/CandidateManagementTests.swift
@@ -24,15 +24,101 @@ final class CandidateManagementTests: XCTestCase {
 
     func testcreateURLRequest() throws {
         //Given
-        let url = URL(string: "https//exemple.com")!
-        var request = URLRequest(url: url)
-        let token =  "fnjkzebrgnkjzberg"
+        let url = "https://example.com/createCandidate"
+            let method = "POST"
+            let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+            let id = "123456"
         
         //When
-        let createURLRequest = try CandidateManagement.createNewCandidateRequest(
-            url: "https://example.com/createCandidate",
-            method: "POST",
-            token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c", id: "123456",
+        let createURLRequest = try CandidateManagement.createURLRequest(url: url, method: method, token: token, id: id)
+
+        //Then
+        XCTAssertEqual(createURLRequest.httpMethod, "POST")
+        XCTAssertEqual(createURLRequest.url?.absoluteString, url)
+        XCTAssertEqual(createURLRequest.allHTTPHeaderFields?["Authorization"], "Bearer \(token)")
+        XCTAssertEqual(createURLRequest.allHTTPHeaderFields?["Content-Type"], "application/json")
+        
+    }
+    func testInvalidcreateURLRequest() throws {
+        //Given
+            let url = ""
+            let method = "POST"
+            let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+            let id = "123456"
+            
+            //When
+        XCTAssertThrowsError(try CandidateManagement.createURLRequest(url: url, method: method, token: token, id: id)){ error in
+            //Then
+            XCTAssertEqual((error as Error)._code, URLError.badURL.rawValue)
+        }
+    }
+    
+    
+    func testloadCandidatesFromURL() throws {
+        //Given
+        let url = "https://example.com/createCandidate"
+            let method = "POST"
+            let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+            let id = "123456"
+        
+        //When
+        let loadCandidatesFromURL = try CandidateManagement.loadCandidatesFromURL(url: url, method: method, token: token)
+
+        //Then
+        XCTAssertEqual(loadCandidatesFromURL.httpMethod, "POST")
+        XCTAssertEqual(loadCandidatesFromURL.url?.absoluteString, url)
+        XCTAssertEqual(loadCandidatesFromURL.allHTTPHeaderFields?["Authorization"], "Bearer \(token)")
+        XCTAssertEqual(loadCandidatesFromURL.allHTTPHeaderFields?["Content-Type"], "application/json")
+    }
+    func testInvalidloadCandidatesFromURL() throws {
+        //Given
+            let url = ""
+            let method = "POST"
+            let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+            let id = "123456"
+            
+            //When
+        XCTAssertThrowsError(try CandidateManagement.loadCandidatesFromURL(url: url, method: method, token: token)){ error in
+            //Then
+            XCTAssertEqual((error as Error)._code, URLError.badURL.rawValue)
+        }
+    }
+    func testcreateNewCandidateRequest() throws {
+        //Given
+        let url =  "https://example.com/createCandidate"
+        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        let method = "POST"
+        
+        
+        struct EncodeCandidateInformation: Identifiable, Encodable,Hashable{
+            var phone, note: String?
+            var id, firstName: String
+            var linkedinURL: String?
+            var isFavorite: Bool
+            var email, lastName: String
+        }
+        
+        var phone = "+1234567890"
+        var note = "This is a test note for the candidate"
+        var id = "987654321"
+        var firstName = "John"
+        var linkedinURL = "https://www.linkedin.com/in/john-doe"
+        var isFavorite = true
+        var email = "john.doe@example.com"
+        var lastName = "Doe"
+
+        let urlRequest = URL(string:url)!
+        var request = URLRequest(url: urlRequest)
+        request.httpMethod = method
+        let data = EncodeCandidateInformation(phone: phone, note: note, id: id, firstName: firstName, linkedinURL: linkedinURL, isFavorite: isFavorite, email: email, lastName: lastName)
+        let body = try JSONEncoder().encode(data)
+        
+        //When
+        let loadCandidatesFromURL = try CandidateManagement.createNewCandidateRequest(
+            url: url,
+            method: method,
+            token:token,
+            id: id,
             phone: "+1234567890",
             note: "Candidate seems highly qualified",
             firstName: "Alice",
@@ -41,21 +127,12 @@ final class CandidateManagementTests: XCTestCase {
             email: "alice@example.com",
             lastName: "Doe")
 
-        //Then
-        XCTAssertEqual(createURLRequest.httpMethod,"POST")
-        XCTAssertNotNil(createURLRequest.httpMethod)
-        XCTAssertNotNil(createURLRequest.allHTTPHeaderFields)
-        XCTAssertNotNil(createURLRequest.url)
-        
-        
-    }
 
-    func testloadCandidatesFromURL() throws {
-       
-    }
-    
-    func testcreateNewCandidateRequest() throws {
-       
+        //Then
+        XCTAssertEqual(loadCandidatesFromURL.httpMethod, "POST")
+        XCTAssertEqual(loadCandidatesFromURL.url?.absoluteString, url)
+        XCTAssertEqual(loadCandidatesFromURL.allHTTPHeaderFields?["Authorization"], "Bearer \(token)")
+        XCTAssertEqual(loadCandidatesFromURL.allHTTPHeaderFields?["Content-Type"], "application/json")
     }
    
 }
