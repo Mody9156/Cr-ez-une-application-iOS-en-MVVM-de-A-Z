08f7925cdbef35307159478d05b2b2e7a730b29b
Modibo KEITA <modykeita2@gmail.com>
2024-06-12 12:56:24 +0200

    update warning

--- Vitesse/Vitesse/View/CandidateDetailsView.swift
+++ Vitesse/Vitesse/View/CandidateDetailsView.swift
@@ -26,7 +26,7 @@ struct CandidateDetailView: View {
                         if isButtonVisible {
                             Button {
                                 Task {
-                                    try await candidateListViewModel.showFavoriteCandidates(selectedCandidateId: candidateInformation.id)
+                                    try await _ = candidateListViewModel.showFavoriteCandidates(selectedCandidateId: candidateInformation.id)
                                     withAnimation {
                                         isButtonVisible = false
                                     }
