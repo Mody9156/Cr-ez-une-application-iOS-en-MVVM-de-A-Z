bd5e1d20efef4bc76b224476e16459556880221d
Modibo KEITA <modykeita2@gmail.com>
2024-06-12 14:30:25 +0200

    add new function tess

--- Vitesse/VitesseTests/Model/Candidate/CandidateManagementTests.swift
+++ Vitesse/VitesseTests/Model/Candidate/CandidateManagementTests.swift
@@ -6,43 +6,32 @@
 //
 
 import XCTest
+@testable import Vitesse
 
 final class CandidateManagementTests: XCTestCase {
 
-    override func setUp()  {
+    var candidateManagement : CandidateManagement!
+    
+    override func setUp() {
         super.setUp()
-        candidateDataManager = CandidateDataManager(httpService: MockHTTPService())
+        candidateManagement = CandidateManagement()
     }
 
-    override func tearDown() throws {
+    override func tearDown() {
         super.tearDown()
-        candidateDataManager = nil
+        candidateManagement = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testcreateURLRequest() throws {
+       
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testloadCandidatesFromURL() throws {
+       
     }
-    // Mock HTTPService utilisé pour simuler les réponses HTTP
-    class MockHTTPService: HTTPService {
-          
-          var mockResult: (Data, HTTPURLResponse)?
-          
-          func request(_ request: URLRequest) async throws -> (Data, HTTPURLResponse) {
-              guard let result = mockResult else {
-                  throw NSError(domain: "", code: 0, userInfo: nil)
-              }
-              return result
-          }
-      }
+    
+    func testcreateNewCandidateRequest() throws {
+       
+    }
+   
 }
