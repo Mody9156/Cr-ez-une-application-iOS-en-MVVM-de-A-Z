e5c2a635984ebc63f0ca6b55423d87c958b0c1e7
Modibo KEITA <modykeita2@gmail.com>
2024-06-12 14:38:33 +0200

    add XCTAssert

--- Vitesse/VitesseTests/Model/Candidate/CandidateManagementTests.swift
+++ Vitesse/VitesseTests/Model/Candidate/CandidateManagementTests.swift
@@ -23,7 +23,31 @@ final class CandidateManagementTests: XCTestCase {
     }
 
     func testcreateURLRequest() throws {
-       
+        //Given
+        let url = URL(string: "https//exemple.com")!
+        var request = URLRequest(url: url)
+        let token =  "fnjkzebrgnkjzberg"
+        
+        //When
+        let createURLRequest = try CandidateManagement.createNewCandidateRequest(
+            url: "https://example.com/createCandidate",
+            method: "POST",
+            token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c", id: "123456",
+            phone: "+1234567890",
+            note: "Candidate seems highly qualified",
+            firstName: "Alice",
+            linkedinURL: "https://www.linkedin.com/in/alice-example",
+            isFavorite: true,
+            email: "alice@example.com",
+            lastName: "Doe")
+
+        //Then
+        XCTAssertEqual(createURLRequest.httpMethod,"POST")
+        XCTAssertNotNil(createURLRequest.httpMethod)
+        XCTAssertNotNil(createURLRequest.allHTTPHeaderFields)
+        XCTAssertNotNil(createURLRequest.url)
+        
+        
     }
 
     func testloadCandidatesFromURL() throws {
