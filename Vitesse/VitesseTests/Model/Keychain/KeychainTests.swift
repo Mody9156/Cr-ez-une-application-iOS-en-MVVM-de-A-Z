ff4678fed5999cccce2999d0963fdbf4922b5dea
Modibo KEITA <modykeita2@gmail.com>
2024-06-18 18:30:23 +0200

    update testkeychain

--- Vitesse/VitesseTests/Model/Keychain/KeychainTests.swift
+++ Vitesse/VitesseTests/Model/Keychain/KeychainTests.swift
@@ -9,21 +9,37 @@ import XCTest
 @testable import Vitesse
 final class KeychainTests: XCTestCase {
 
-    func testExample() throws {
+    func testkeychain() throws {
         //Given
        let keychain = Keychain()
         //When
         do{
-           try keychain.add("exemple_of_token", forKey: "exemple_1")
-            XCTAssertEqual(keychain.get(forKey: "exemple_1"),)
-        }catch let error as Keychain.KeychainError {
-            XCTAssertEqual(error, .getFailed,"Failed to insert item into keychain.")
-        }
-       
+       try keychain.add("exemple_of_token", forKey: "exemple_1")
+         
         //Then
-        
+            XCTAssertNoThrow( try keychain.add("exemple_of_token", forKey: "exemple_1"))
+            
+        }catch  {
+            XCTFail("Unexpected error: \(error)")        }
+       
     }
-
+//    func testInvalid_keychain() throws {
+//        //Given
+//       let keychain = Keychain()
+//        //When
+//        do{
+//         let add =  try keychain.add("exemple_of_token", forKey: "")
+//         let get =  try keychain.get(forKey: "exemple_1")
+//            guard let encodingToken = String(data: get, encoding: .utf8) else {
+//                throw CandidateManagementError.fetchTokenError
+//            }
+//
+//            XCTAsserttrue(encodingToken.isEmpty)
+//        }catch let error as Keychain.KeychainError {
+//            XCTAssertEqual(error, .insertFailed,"Failed to insert item into keychain.")
+//        }
+//
+//    }
     func testPerformanceExample() throws {
       
     }
